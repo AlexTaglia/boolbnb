2b0f1eeac7a1c6dddf8029ef73fc3d8dd729bf72
AlexTaglia <alex.tagliabue@gmail.com>
2021-10-21 10:36:28 +0200

    small bug fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('apartment.index')}}">I miei appartementi</a>
+                                <a class="dropdown-item" href="{{ route('apartment.index')}}">I miei appartamenti</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
